981c5bc2f45be2aa4b3ac6e59cac527900b0b615
AdiBier <AdiBier@interia.eu>
2019-01-26 20:41:56 +0100

    Header - widoczki Repo - zabiegi

--- src/main/java/com/example/demo/config/RepositoriesInitializer.java
+++ src/main/java/com/example/demo/config/RepositoriesInitializer.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import com.example.demo.models.Patient;
 import com.example.demo.models.Role;
+import com.example.demo.models.Treatment;
 import com.example.demo.models.User;
 import com.example.demo.repositories.*;
 import org.springframework.beans.factory.InitializingBean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -69,6 +71,42 @@ public class RepositoriesInitializer {
                     admin.setPatient(adminPatient);
                     admin.setRoles(new HashSet<>(Arrays.asList(roleDentist, roleUser, roleAdmin)));
                     admin.setPassword(passwordEncoder.encode("admin"));
+
+                    Treatment treatment1 = new Treatment();
+                    treatment1.setName("Wybielenie zębów");
+                    treatment1.setPrice(new BigDecimal(1500));
+                    treatment1.setActive(true);
+                    treatmentRepository.save(treatment1);
+
+                    Treatment treatment2 = new Treatment();
+                    treatment2.setName("Wyrwanie zęba");
+                    treatment2.setPrice(new BigDecimal(50));
+                    treatment2.setActive(true);
+                    treatmentRepository.save(treatment2);
+
+                    Treatment treatment3 = new Treatment();
+                    treatment3.setName("Kanałowe leczenie");
+                    treatment3.setPrice(new BigDecimal(750));
+                    treatment3.setActive(true);
+                    treatmentRepository.save(treatment3);
+
+                    Treatment treatment4 = new Treatment();
+                    treatment4.setName("Plomba");
+                    treatment4.setPrice(new BigDecimal(75));
+                    treatment4.setActive(true);
+                    treatmentRepository.save(treatment4);
+
+                    Treatment treatment5 = new Treatment();
+                    treatment5.setName("Prześwietlenie zęba");
+                    treatment5.setPrice(new BigDecimal(25));
+                    treatment5.setActive(true);
+                    treatmentRepository.save(treatment5);
+
+                    Treatment treatment6 = new Treatment();
+                    treatment6.setName("Piaskowanie zębów");
+                    treatment6.setPrice(new BigDecimal(200));
+                    treatment6.setActive(true);
+                    treatmentRepository.save(treatment6);
 
                     userRepository.save(user);
                     userRepository.save(admin);
